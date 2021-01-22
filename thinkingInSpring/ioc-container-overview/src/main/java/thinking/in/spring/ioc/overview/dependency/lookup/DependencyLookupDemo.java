fd9f15d2cb3f0f086b6e59b2e991653bb44adda9
Adrian Xu <741996133@qq.com>
2021-01-22 23:59:30 +0800

    ioc

---
+++ thinkingInSpring/ioc-container-overview/src/main/java/thinking/in/spring/ioc/overview/dependency/lookup/DependencyLookupDemo.java
@@ -0,0 +1,10 @@
+package thinking.in.spring.ioc.overview.dependency.lookup;
+
+import org.springframework.beans.factory.BeanFactory;
+
+public class DependencyLookupDemo {
+    public static void main(String[] args) {
+//        BeanFactory beanFactory = new ClassPathXm
+//        SpringContext
+    }
+}
